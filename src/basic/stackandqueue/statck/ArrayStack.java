8a184c4bb2fe340b0dae194a2f446b752993af7f
wangjianhua <wangjianhuabox@163.com>
2021-04-23 18:07:00 +0800

    加入受检异常

--- src/basic/stackandqueue/statck/ArrayStack.java
+++ src/basic/stackandqueue/statck/ArrayStack.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
  * @author wangjianhua
  * @date 2021-04-23 16:53
  */
+@SuppressWarnings("unchecked")
 public class ArrayStack<Item> implements MyStack<Item> {
     /**
      *
